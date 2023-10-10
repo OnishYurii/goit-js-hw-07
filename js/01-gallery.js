575dc278db1fe060c2f8c6c07a72e3e76821b6cf
OnishYurii <onishchenkoyurii@gmail.com>
2023-10-10 18:23:45 +0300

    First Task Done

---
+++ js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<li class='gallery__item'>
+             <a class="gallery__link" href=${original}>
+               <img class="gallery__image" src=${preview} alt="${description}" data-source=${original} />
+             </a>
+       </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+gallery.addEventListener("click", openModal);
+
+let fullSizeImg;
+
+function openModal(event) {
+  event.preventDefault();
+  let imageRef;
+
+  if (event.target.nodeName === "IMG") {
+    imageRef = event.target.dataset.source;
+  } else {
+    return;
+  }
+  console.log(imageRef);
+
+  fullSizeImg = basicLightbox.create(
+    `
+    <img src=${imageRef} />`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", modalCloseByEscape);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", modalCloseByEscape);
+      },
+    }
+  );
+
+  fullSizeImg.show();
+}
+
+function modalCloseByEscape({ code }) {
+  if (code === "Escape") {
+    fullSizeImg.close();
+  }
+}
